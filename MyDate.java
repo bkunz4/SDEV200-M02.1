4f4d918dd07447624e7b478c3b452551adaf6923
bkunz4 <136372473+bkunz4@users.noreply.github.com>
2023-11-03 14:59:08 +0000

    final

--- MyDate.java
+++ MyDate.java
@@ -1,51 +1,59 @@
 import java.util.Date;
 import java.util.Calendar;
 
+//Data Fields
 public class MyDate {
     private int year;
-    private int month;  // 0-based, i.e., 0 for January
+    private int month;  
     private int day;
 
-    // No-arg constructor that creates a MyDate object for the current date
-    public MyDate() {
-        Date currentDate = new Date();
+  // No-arg constructor that creates a MyDate object for the current date
+    public MyDate() {  
+      Date currentDate = new Date();
         long currentTime = currentDate.getTime();
         setDate(currentTime);
     }
-
-    // Constructor that constructs a MyDate object with a specified elapsed time
-    // since midnight, January 1, 1970, in milliseconds.
+// Constructor that constructs a MyDate object with a specified elapsed time since midnight, January 1, 1970, in milliseconds.
     public MyDate(long elapsedTime) {
         setDate(elapsedTime);
     }
-
-    // Constructor that constructs a MyDate object with the specified year, month, and day
+// Constructor that constructs a MyDate object with the specified year, month, and day
     public MyDate(int year, int month, int day) {
         this.year = year;
         this.month = month;
         this.day = day;
     }
 
-    // Getter method for the year
+  // Getter method for the year
     public int getYear() {
         return year;
     }
 
-    // Getter method for the month
+// Getter method for the month
     public int getMonth() {
         return month;
     }
 
-    // Getter method for the day
+// Getter method for the day
     public int getDay() {
         return day;
     }
 
-    // Method to set a new date for the object using elapsed time
+// Sets a new date for the object using elapsed time
     public void setDate(long elapsedTime) {
         Date date = new Date(elapsedTime);
         year = calendar.get(Calendar.YEAR);
         month = calendar.get(Calendar.MONTH); 
         day = calendar.get(Calendar.DAY_OF_MONTH); 
-    } 
+    }
+// Test program
+public static void main(String[] args) {
+  // Creates two MyDate objects
+  MyDate date1 = new MyDate();
+  MyDate date2 = new MyDate(34355555133101L);
+// Prints results
+  System.out.println("Date1: " + date1.getMonth() + "/" + date1.getDay() + "/" + date1.getYear());
+  System.out.println("Date2: " + date2.getMonth() + "/" + date2.getDay() + "/" + date2.getYear());
+}
+     
 }
